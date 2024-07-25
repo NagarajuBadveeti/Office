161c3bd69a87aff9407be6efba06612451f47d3d
NagarajuBadveeti <badveetiraju@gmail.com>
2024-07-25 16:13:02 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+ 
+const port = 5000
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req,res) =>{
+    res.send("hello world");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
